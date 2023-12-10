1d0812a53e0ed7811a2d11a3e24cd60efd97d68b
Muhammad Ishaan <iftekharctgbd@gmail.com>
2023-12-10 21:01:19 +0600

    client side messag send logic added

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ class SocketService {
   private _io: Server;
 
   constructor() {
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
   }
 
   get io() {
